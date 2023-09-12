d632a6841de63951df3f6190aad43c4e7c504bdb
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-09-12 19:23:31 -0400

    :sparkles: function to convert ms to mm:ss cuz Date wasn't behaving

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -4,3 +4,23 @@ export function convertLowerCaseToPascalCase(text: string): string {
         .map((str) => str[0].toUpperCase() + str.slice(1).toLowerCase())
         .join(" ");
 }
+
+/*
+    author: ChatGPT
+*/
+export function millisecondsToMinutesAndSeconds(milliseconds: number): string {
+    // Calculate the total seconds
+    const totalSeconds: number = Math.floor(milliseconds / 1000);
+
+    // Calculate the minutes and seconds
+    const minutes: number = Math.floor(totalSeconds / 60);
+    const seconds: number = totalSeconds % 60;
+
+    // Format the result as "mm:ss"
+    const formattedMinutes: string =
+        minutes < 10 ? `${minutes}` : minutes.toString();
+    const formattedSeconds: string =
+        seconds < 10 ? `0${seconds}` : seconds.toString();
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
